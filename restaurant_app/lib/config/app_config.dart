7c0db379b6e053c923408933404071208fc78d07
Fennikusu <simondubicki@gmail.com>
2025-05-06 09:20:59 +0200

    project cleaning and last push

--- restaurant_app/lib/config/app_config.dart
+++
@@ -1,8 +0,0 @@
-/// Application configuration constants
-class AppConfig {
-  /// API URL
-  static const String apiUrl = 'https://raw.githubusercontent.com/popina/test-flutter/main/data.json';
-  
-  /// App name
-  static const String appName = 'Restaurant App';
-}
